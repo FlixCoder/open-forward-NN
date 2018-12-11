8b7d26ec784d795e63e3383b54d9b72956c5fe31
FlixCoder <xbbfanx@web.de>
2018-12-11 19:38:08 +0100

    First implementation [untested]

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # open-forward-NN
-An open (accessible parameters), feed-forward-only NN (neural net) library to use it in an dynamic and free way in evolutionary algorithms
+An open (accessible parameters), feed-forward-only NN (neural net) library to use it in an dynamic and free way in evolutionary algorithms.
+
+## Usage
+Take a look at the examples.
---
+++ src/lib.rs
@@ -0,0 +1,480 @@
+//! @author = FlixCoder
+//!
+//! Architecture influenced by Keras: Sequential models
+
+#[macro_use]
+extern crate serde_derive;
+
+extern crate serde;
+extern crate serde_json;
+extern crate rand;
+
+use std::io::prelude::*;
+use std::fs::File;
+use rand::Rng;
+use rand::distributions::{Normal, Distribution};
+
+//TODO:
+//error functions (MAE, MSE, cross-entropy, logcosh, ..)
+//add PELU
+//add batch norm? (using running average)
+
+
+//values for a Normal(0, 1) distribution from https://arxiv.org/pdf/1706.02515.pdf
+const SELU_LAMBDA:f64 = 1.0507;
+const SELU_ALPHA:f64 = 1.6733;
+//values for a Normal(0, 2) distribution from https://arxiv.org/pdf/1706.02515.pdf
+//const SELU_LAMBDA:f64 = 1.06071;
+//const SELU_ALPHA:f64 = 1.97126;
+
+
+/// Define the available types of layers
+#[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
+pub enum Layer
+{
+    //Activation functions
+    /// linear activation
+    Linear,
+    /// rectified linear unit
+    ReLU,
+    /// leaky rectified linear unit (factor = factor to apply for x < 0)
+    LReLU(f64),
+    /// exponential linear unit (alpha = 1)
+    ELU,
+    /// scaled exponential linear unit (self-normalizing). parameters are adapted to var=1 data
+    SELU,
+    /// sigmoid
+    Sigmoid,
+    /// tanh
+    Tanh,
+    /// quadratic
+    Quadratic,
+    /// cubic
+    Cubic,
+    /// clipped linear activation [-1, 1]
+    ClipLinear,
+    /// gaussian
+    Gaussian,
+    /// soft plus
+    SoftPlus,
+    /// soft max
+    SoftMax,
+    
+    //Regularization / Normalization
+    /// Apply dropout to the previous layer (d = percent of neurons to drop)
+    Dropout(f64),
+    
+    //Neuron-layers
+    /// Dense layer (params = weights of the layer, be sure to have the correct dimensions! include bias as first parameter)
+    Dense(Vec<Vec<f64>>),
+}
+
+/// Definition of usable initializers in Sequential.add_layer_dense
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub enum Initializer
+{
+    /// Glorot/Xavier initialization
+    Glorot,
+    /// He initialization
+    He,
+    /// initialize with a constant value
+    Const(f64),
+}
+
+
+/// Implementation of the neural network / sequential models of layers
+#[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
+pub struct Sequential
+{
+    num_inputs:usize,
+    layers:Vec<Layer>,
+    num_outputs:usize,
+}
+
+impl Sequential
+{
+    /// Create a new instance of a sequential model
+    /// num_inputs = the number of inputs to the model
+    pub fn new(num_inputs:usize) -> Sequential
+    {
+        Sequential { num_inputs: num_inputs, layers: Vec::new(), num_outputs: num_inputs }
+    }
+    
+    /// Returns the requested input dimension
+    pub fn get_num_inputs(&self) -> usize
+    {
+        self.num_inputs
+    }
+    
+    /// Get the layers (as ref)
+    pub fn get_layers(&self) -> &Vec<Layer>
+    {
+        &self.layers
+    }
+    
+    /// Get the layers (as mut)
+    pub fn get_layers_mut(&mut self) -> &mut Vec<Layer>
+    {
+        &mut self.layers
+    }
+    
+    /// Return the flat parameters of the layers (including LReLU factors).
+    /// Used for evolution-strategies
+    pub fn get_params(&self) -> Vec<f64>
+    {
+        let mut params = Vec::new();
+        for layer in self.layers.iter()
+        {
+            match layer
+            {
+                //Activation functions
+                Layer::LReLU(factor) => params.push(*factor),
+                //Regularization / Normalization
+                //Layer::Dropout(d) => params.push(*d),
+                //Neuron-layers
+                Layer::Dense(weights) =>
+                {
+                    for vec in weights.iter()
+                    {
+                        for val in vec.iter()
+                        {
+                            params.push(*val);
+                        }
+                    }
+                },
+                //rest does not have params
+                _ => (),
+            }
+        }
+        params
+    }
+    
+    /// Set the layers' parameters (including LReLU factors) by a flat input.
+    /// Used for evolution-strategies.
+    /// Panics if params' size does not fit the layers
+    pub fn set_params(&mut self, params:&Vec<f64>) -> &mut Self
+    {
+        let mut iter = params.iter();
+        for layer in self.layers.iter_mut()
+        {
+            match layer
+            {
+                //Activation functions
+                Layer::LReLU(factor) => *factor = *iter.next().expect("Vector params is not big enough!"),
+                //Regularization / Normalization
+                //Layer::Dropout(d) => *d = *iter.next().expect("Vector params is not big enough!"),
+                //Neuron-layers
+                Layer::Dense(weights) =>
+                {
+                    for vec in weights.iter_mut()
+                    {
+                        for val in vec.iter_mut()
+                        {
+                            *val = *iter.next().expect("Vector params is not big enough!");
+                        }
+                    }
+                },
+                //rest does not have params
+                _ => (),
+            }
+        }
+        self
+    }
+    
+    /// Add a layer to the sequential model. Be sure to have appropriate parameters inside the layer, they are not checked!
+    /// You can use specific add_layer_<layer> methods to get simple, correct creation of layers with parameters.
+    pub fn add_layer(&mut self, layer:Layer) -> &mut Self
+    {
+        match &layer
+        {
+            Layer::Dense(weights) => self.num_outputs = weights.len(),
+            _ => (),
+        }
+        self.layers.push(layer);
+        self
+    }
+    
+    /// Add a LReLU layer:
+    /// factor = factor to apply to x < 0
+    pub fn add_layer_lrelu(&mut self, factor:f64) -> &mut Self
+    {
+        let layer = Layer::LReLU(factor);
+        self.layers.push(layer);
+        self
+    }
+    
+    /// Add a Dropout layer:
+    /// d = fraction of nodes to drop
+    pub fn add_layer_dropout(&mut self, d:f64) -> &mut Self
+    {
+        if d < 0.0 || d >= 1.0
+        {
+            panic!("Inappropriate dropout parameter!");
+        }
+        
+        let layer = Layer::Dropout(d);
+        self.layers.push(layer);
+        self
+    }
+    
+    /// Add a Dense layer:
+    /// neurons = number of neurons/units in the layer
+    /// init = initializer to use
+    pub fn add_layer_dense(&mut self, neurons:usize, init:Initializer) -> &mut Self
+    {
+        let weights = match init
+        {
+            Initializer::Glorot => gen_glorot(self.num_outputs, neurons),
+            Initializer::He => gen_he(self.num_outputs, neurons),
+            Initializer::Const(val) => vec![vec![val; self.num_outputs+1]; neurons],
+        };
+        self.num_outputs = neurons;
+        let layer = Layer::Dense(weights);
+        self.layers.push(layer);
+        self
+    }
+    
+    /// Do a forward pass through the model
+    pub fn run(&self, input:&Vec<f64>) -> Vec<f64>
+    {
+        if input.len() != self.num_inputs
+        {
+            panic!("Incorrect input size!");
+        }
+        
+        let mut result = input.clone();
+        for layer in self.layers.iter()
+        {
+            match layer
+            {
+                //Activation functions
+                Layer::Linear => result.iter_mut().for_each(|x| { *x = linear(*x); }),
+                Layer::ReLU => result.iter_mut().for_each(|x| { *x = relu(*x); }),
+                Layer::LReLU(factor) => result.iter_mut().for_each(|x| { *x = lrelu(*x, *factor); }),
+                Layer::ELU => result.iter_mut().for_each(|x| { *x = elu(*x); }),
+                Layer::SELU => result.iter_mut().for_each(|x| { *x =selu(*x); }),
+                Layer::Sigmoid => result.iter_mut().for_each(|x| { *x = sigmoid(*x); }),
+                Layer::Tanh => result.iter_mut().for_each(|x| { *x = tanh(*x); }),
+                Layer::Quadratic => result.iter_mut().for_each(|x| { *x = quadratic(*x); }),
+                Layer::Cubic => result.iter_mut().for_each(|x| { *x = cubic(*x); }),
+                Layer::ClipLinear => result.iter_mut().for_each(|x| { *x = clip_linear(*x); }),
+                Layer::Gaussian => result.iter_mut().for_each(|x| { *x = gaussian(*x); }),
+                Layer::SoftPlus => result.iter_mut().for_each(|x| { *x = softplus(*x); }),
+                Layer::SoftMax => softmax(&mut result),
+                
+                //Regularization / Normalization
+                Layer::Dropout(d) => apply_dropout(&mut result, *d),
+                
+                //Neuron-layers
+                Layer::Dense(weights) => result = modified_matrix_dotprod(&weights, &result),
+            }
+        }
+        result
+    }
+    
+    /// Predict values (forward pass) for a vector of input data
+    pub fn predict(&self, inputs:&Vec<Vec<f64>>) -> Vec<Vec<f64>>
+    {
+        let mut results = Vec::new();
+        for input in inputs.iter()
+        {
+            let result = self.run(input);
+            results.push(result);
+        }
+        results
+    }
+    
+    /// Encodes the model as a JSON string.
+    pub fn to_json(&self) -> String
+    {
+        serde_json::to_string(self).expect("Encoding JSON failed!")
+    }
+
+    /// Builds a new model from a JSON string.
+    pub fn from_json(encoded:&str) -> Sequential
+    {
+        serde_json::from_str(encoded).expect("Decoding JSON failed!")
+    }
+    
+    /// Saves the model to a file
+    pub fn save(&self, file:&str) -> Result<(), std::io::Error>
+    {
+        let mut file = File::create(file)?;
+        let json = self.to_json();
+        file.write_all(json.as_bytes())?;
+        Ok(())
+    }
+    
+    /// Creates a model from a file
+    pub fn load(file:&str) -> Result<Sequential, std::io::Error>
+    {
+        let mut file = File::open(file)?;
+        let mut json = String::new();
+        file.read_to_string(&mut json)?;
+        Ok(Sequential::from_json(&json))
+    }
+}
+
+
+
+//helper functions
+/// Generate a vector of random numbers with 0 mean and std std, normally distributed.
+fn gen_rnd_vec(n:usize, std:f64) -> Vec<f64>
+{
+    let mut rng = rand::thread_rng();
+    let normal = Normal::new(0.0, std);
+    normal.sample_iter(&mut rng).take(n).collect()
+}
+
+/// Generate parameters based on Glorot initialization
+fn gen_glorot(n_in:usize, n_out:usize) -> Vec<Vec<f64>>
+{
+    let std = (2.0 / (n_in + n_out) as f64).sqrt();
+    let mut weights = Vec::new();
+    for _ in 0..n_out
+    {
+        weights.push(gen_rnd_vec(n_in + 1, std));
+    }
+    weights
+}
+
+/// Generate parameters based on He initialization
+fn gen_he(n_in:usize, n_out:usize) -> Vec<Vec<f64>>
+{
+    let std = (2.0 / n_in as f64).sqrt();
+    let mut weights = Vec::new();
+    for _ in 0..n_out
+    {
+        weights.push(gen_rnd_vec(n_in + 1, std));
+    }
+    weights
+}
+
+/// Apply dropout to a layer. d = fraction of nodes to be dropped
+fn apply_dropout(layer:&mut Vec<f64>, d:f64)
+{
+    if d == 0.0
+    { //allow zero dropout to allow later change, but do nothing here
+        return;
+    }
+    // set nodes to zero
+    let num = (d * layer.len() as f64) as usize;
+    let mut rng = rand::thread_rng();
+    for _ in 0..num
+    {
+        let i = rng.gen::<usize>() % layer.len();
+        layer[i] = 0.0;
+    }
+    //divide other nodes by probability to adapt variance
+    layer.iter_mut().for_each(|x| { *x /= d; })
+}
+
+/// Calculate layer results with bias from weight
+/// If weights matrix is empty, result will be empty (indicating zero nodes)
+fn modified_matrix_dotprod(weights:&Vec<Vec<f64>>, values:&Vec<f64>) -> Vec<f64>
+{
+    let mut result = Vec::new();
+    for node in weights.iter()
+    {
+        let mut iter = node.iter();
+        let mut sum = *iter.next().expect("Empty weights! (Bias)");
+        for (weight, value) in iter.zip(values.iter()) //panics if weights do not have the correct shape
+        {
+            sum += weight * value;
+        }
+        result.push(sum);
+    }
+    result
+}
+
+
+//activiation functions
+fn linear(x:f64) -> f64
+{
+    x
+}
+
+fn relu(x:f64) -> f64
+{
+    x.max(0.0)
+}
+
+fn lrelu(x:f64, factor:f64) -> f64
+{
+    if x < 0.0
+    {
+        factor * x
+    }
+    else
+    {
+        x
+    }
+}
+
+fn elu(x:f64) -> f64
+{
+    if x < 0.0
+    {
+        x.exp()
+    }
+    else
+    {
+        x
+    }
+}
+
+fn selu(x:f64) -> f64
+{
+    SELU_LAMBDA * if x < 0.0
+    {
+        SELU_ALPHA * x.exp() - SELU_ALPHA
+    }
+    else
+    {
+        x
+    }
+}
+
+fn sigmoid(x:f64) -> f64
+{
+    1.0 / (1.0 + (-x).exp())
+}
+
+fn tanh(x:f64) -> f64
+{
+    x.tanh()
+}
+
+fn quadratic(x:f64) -> f64
+{
+    x * x
+}
+
+fn cubic(x:f64) -> f64
+{
+    x * x * x
+}
+
+fn clip_linear(x:f64) -> f64
+{
+    x.min(1.0).max(-1.0)
+}
+
+fn gaussian(x:f64) -> f64
+{
+    (-x * x).exp()
+}
+
+fn softplus(x:f64) -> f64
+{
+    (1.0 + x.exp()).ln()
+}
+
+fn softmax(arr:&mut Vec<f64>)
+{
+    let norm:f64 = arr.iter().map(|x| x.exp()).sum();
+    for val in arr.iter_mut()
+    {
+        *val = val.exp() / norm;
+    }
+}
